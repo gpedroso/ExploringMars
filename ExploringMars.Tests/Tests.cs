7ab290726f396b2f5211def4e2a84a85352d55d3
gabriel Pedroso <gpedroso@gmail.com>
2021-11-23 22:58:12 -0300

    TestSucessPass

--- ExploringMars.Tests/Tests.cs
+++ ExploringMars.Tests/Tests.cs
@@ -20,5 +20,7 @@ namespace ExploringMars.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+
     }
 }
--- ExploringMars/Control.cs
+++ ExploringMars/Control.cs
@@ -8,8 +8,48 @@ namespace ExploringMars
     {
         public static string ControlProbe((int X, int Y) spaceLimit, (int X, int Y, char direction) positionProbe, string pathProbe)
         {
+            foreach (char move in pathProbe)
+            {
+                if (move == 'M')
+                {
+                    if (positionProbe.direction == 'N')
+                        positionProbe.Y += 1;
+                    if (positionProbe.direction == 'S')
+                        positionProbe.Y -= 1;
+                    if (positionProbe.direction == 'E')
+                        positionProbe.X += 1;
+                    if (positionProbe.direction == 'W')
+                        positionProbe.X -= 1;
+                }
 
-            return "";
+                if (move == 'L')
+                {
+                    if (positionProbe.direction == 'N')
+                        positionProbe.direction = 'W';
+                    if (positionProbe.direction == 'W')
+                        positionProbe.direction = 'S';
+                    if (positionProbe.direction == 'S')
+                        positionProbe.direction = 'E';
+                    if (positionProbe.direction == 'E')
+                        positionProbe.direction = 'N';
+                }
+
+                if (move == 'R')
+                {
+                    if (positionProbe.direction == 'N')
+                        positionProbe.direction = 'E';
+                    if (positionProbe.direction == 'E')
+                        positionProbe.direction = 'S';
+                    if (positionProbe.direction == 'S')
+                        positionProbe.direction = 'W';
+                    if (positionProbe.direction == 'W')
+                        positionProbe.direction = 'N';
+                }
+
+            }
+
+
+            return $"{positionProbe.X.ToString()} {positionProbe.Y.ToString()} {positionProbe.direction}";
         }
     }
 }
